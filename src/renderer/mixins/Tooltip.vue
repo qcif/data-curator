50d39fc5d74484f9dccfa39a766640f7bd90fc57
Matthew Mulholland <matt@redboxresearchdata.com.au>
2017-09-27 13:57:30 +1000

    replace fill with quote. start trail tooltip.

--- src/renderer/mixins/Tooltip.vue
+++ src/renderer/mixins/Tooltip.vue
@@ -1,10 +1,16 @@
 <script>
 export default {
+  data() {
+    return {
+      tooltipStyle: 'color:#fff;padding-left:20px;'
+    }
+  },
   methods: {
     tooltip: function(property) {
       let object = {}
       object.html = property
       object.visible = property || false
+      // object.style = this.tooltipStyle
       return object
     }
   }
