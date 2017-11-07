140a1b0677ea716f82ce27d4b9ed2eecd0601e4e
Matthew Mulholland <matt@redboxresearchdata.com.au>
2017-11-08 03:37:30 +1000

    OCC-96: updated custom rule name. added logic for resources, checking that resource path unique and adding readme.

--- src/renderer/exportPackage.js
+++ src/renderer/exportPackage.js
@@ -1,7 +1,9 @@
 import fs from 'fs-extra'
-import {
-  remote
-} from 'electron'
+import archiver from 'archiver'
+import {remote} from 'electron'
+import tabStore from '@/store/modules/tabs.js'
+import hotStore from '@/store/modules/hots.js'
+import {extractNameFromFile} from '@/store/tabStoreUtilities.js'
 const Dialog = remote.dialog
 
 export function createZipFile() {
@@ -21,9 +23,43 @@ export function createZipFile() {
 }
 
 function generateDataPackage(filename) {
+  let output = fs.createWriteStream(filename)
+  let archive = archiver('zip', {
+    zlib: {
+      level: 9
+    } // Sets the compression level.
+  })
+  output.on('close', () => {
+    console.log(archive.pointer() + ' total bytes')
+    console.log('archiver has been finalized and the output file descriptor has closed.')
+  }).on('end', () => {
+    console.log('Data has been drained')
+  })
+  archive.on('warning', function(err) {
+    if (err.code === 'ENOENT') {
+      // log warning
+    } else {
+      // throw error
+      throw err
+    }
+  }).on('error', function(err) {
+    throw err
+  })
+  archive.pipe(output)
+  zipResources(archive)
+  zipProvenanceProperties(archive)
+  archive.finalize()
   console.log('ok')
 }
 
-function addProvenanceProperties() {
+function zipResources(archive) {
+  for (let filename of tabStore.getters.getTabFilenames(tabStore.state)) {
+    let name = extractNameFromFile(filename)
+    archive.append(fs.createReadStream(filename), { name: name, prefix: 'data' })
+  }
+}
+
+function zipProvenanceProperties(archive) {
   let provenance = hotStore.state.provenanceProperties.markdown
+  archive.append(provenance, { name: 'Readme.md' })
 }
