2bdd37dd43cc7cbc371572b9a9c3f4d7b09433ad
Matthew Mulholland <matt@redboxresearchdata.com.au>
2017-11-12 09:00:00 +1000

    OCC-61: set up logic for processing csv files and provenance.

--- src/renderer/importPackage.js
+++ src/renderer/importPackage.js
@@ -1,34 +1,85 @@
 import fs from 'fs-extra'
 import path from 'path'
-import tabStore from '@/store/modules/tabs.js'
 import hotStore from '@/store/modules/hots.js'
-import {extractNameFromFile} from '@/store/tabStoreUtilities.js'
+import unzipper from 'unzipper'
+import etl from 'etl'
+import {ipcRenderer as ipc} from 'electron'
 
-export function unzipFile(filename) {
-  console.log(`unzipping ${zipSource}...`)
-  let unzipDestination = path.dirname(zipSource)
-  console.log(`destination dir is ${unzipDestination}`)
+export function unzipFile(zipSource) {
+  unzipFileToDir(zipSource, path.dirname(zipSource))
 }
 
-//  // fs.createReadStream(unzipDestination).pipe(unzip.Extract({ path: destPath }))
-//  fs.createReadStream(zipSource).pipe(unzip.Parse()).on('entry', function(entry) {
-//    var filename = entry.path
-//    var type = entry.type // 'Directory' or 'File'
-//    var size = entry.size
-//    console.log(entry)
-//    console.log(filename)
-//    console.log(type)
-//    console.log(size)
-//    // if (fileName === "this IS the file I'm looking for") {
-//    try {
-//      entry.pipe(fs.createWriteStream(`${unzipDestination}/${filename}`))
-//    } catch (err) {
-//      console.log('There was a problem importing zip')
-//      console.log(err)
-//      entry.autodrain()
-//    }
-//    // } else {
-//    //   entry.autodrain()
-//    // }
-//  })
-// }
+async function unzipFileToDir(zipSource, unzipDestination) {
+  fs.createReadStream(zipSource).pipe(unzipper.Parse()).pipe(etl.map(async entry => {
+    let fileDestination = `${unzipDestination}/${entry.path}`
+    // drain any temp sub-directories created during unzip
+    if (isUnzippedTempDir(zipSource, fileDestination)) {
+      entry.autodrain()
+      return
+    }
+    switch (path.extname(entry.path)) {
+      case '.csv':
+        await ensureFile(fileDestination)
+        await unzippedEntryToFile(entry, fileDestination)
+        openFileIntoTab(fileDestination)
+        break
+      case '.json':
+        await ensureFile(fileDestination)
+        await unzippedEntryToFile(entry, fileDestination)
+        break
+      case '.md':
+        await ensureFile(fileDestination)
+        await unzippedEntryToFile(entry, fileDestination)
+        await setProvenance(fileDestination)
+        break
+      default:
+        entry.autodrain()
+        break
+    }
+  }))
+}
+
+function isUnzippedTempDir(zipSource, fileDestination) {
+  let sourceExtension = path.extname(zipSource)
+  let sourceNoExt = zipSource.substring(0, zipSource.lastIndexOf(sourceExtension))
+  return !_.startsWith(fileDestination, sourceNoExt)
+}
+
+async function unzippedEntryToFile(entry, fileDestination) {
+  try {
+    let returned = await entry.pipe(etl.toFile(fileDestination)).promise()
+    return returned
+  } catch (err) {
+    console.log(`Error unzipping ${fileDestination}`)
+    console.log(err)
+  }
+}
+
+async function ensureFile(fileDestination) {
+  try {
+    await fs.ensureFile(fileDestination)
+  } catch (err) {
+    console.log(`Error touching file destination ${fileDestination}`)
+    console.log(err)
+  }
+}
+
+function openFileIntoTab(filename) {
+  ipc.send('openFileIntoTab', filename)
+}
+
+async function setProvenance(filename) {
+  try {
+    let value = await etl.file(filename)
+      .pipe(etl.stringify())
+      .promise()
+    // etl stringify always returns an array
+    if (_.isEmpty(value)) {
+      return
+    }
+    hotStore.mutations.pushProvenance(hotStore.state, JSON.parse(value).text)
+  } catch (err) {
+    console.log(`Error setting provenance from ${filename}`)
+    console.log(err)
+  }
+}
