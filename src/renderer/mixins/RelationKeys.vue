75e37656ba167cf14cc3552b328d13fb71d47167
Matthew Mulholland <matt@redboxresearchdata.com.au>
2017-11-22 21:10:12 +1000

    stash subscribe tab.

---
+++ src/renderer/mixins/RelationKeys.vue
@@ -0,0 +1,84 @@
+<script>
+import {
+  mapMutations,
+  mapState,
+  mapGetters
+} from 'vuex'
+import { Subscription } from 'rxjs/Subscription'
+import { Subject } from 'rxjs/Subject'
+import {startWith} from 'rxjs/add/operator/startWith.js'
+import VueRx from 'vue-rx'
+import Vue from 'vue'
+import {activeHotAllColumnNames} from '@/rxSubject.js'
+import {
+  HotRegister
+} from '@/hot.js'
+Vue.use(VueRx, {
+  Subscription,
+  Subject,
+  startWith
+})
+export default {
+  name: 'relationkeys',
+  props: ['waitForHotIdFromTabId', 'setProperty', 'getProperty', 'getPropertyGivenHotId', 'propertyName'],
+  // data() {
+  //   return {
+  //     selectedKeys: [],
+  //     activeNames: []
+  //   }
+  // },
+  watch: {
+    // selectedKeys: function(values) {
+    //   this.setProperty(this.propertyName, values)
+    // },
+    getActiveTab: function() {
+      this.initTableHeaderKeys()
+    }
+  },
+  computed: {
+    ...mapGetters(['getActiveTab', 'getAllHotColumnNamesFromHotId', 'getAllHotTablesColumnNames'])
+  },
+  methods: {
+    ...mapMutations(['pushAllColumnsProperty']),
+    // initActiveId: async function() {
+    //   let tab = this.getActiveTab
+    //   let activeId = this.waitForHotIdFromTabId(tab)
+    //   return activeId
+    // },
+    initTableHeaderKeys: function() {
+      let allHotTablesColumnNames = this.getAllHotTablesColumnNames()
+      activeHotAllColumnNames.next(allHotTablesColumnNames)
+    },
+    // updateActiveNames: function(names) {
+    //   this.activeNames = _.without(names, '')
+    // },
+    // updateSelectedKeys: function(hotId) {
+    //   let value = this.getPropertyGivenHotId(this.propertyName, hotId) || []
+    //   this.selectedKeys = value
+    // }
+    updateSubscriptions: function(names, id) {}
+  },
+  subscriptions() {
+    return {
+      vueActiveId: this.waitForHotIdFromTabId()
+      // .startWith(HotRegister.getActiveInstance().guid)
+    }
+  },
+  mounted: function() {
+    // let vueActiveId
+    // let vueActiveId = this.initActiveId
+    // let vueUpdateActiveNames = this.updateActiveNames
+    // let vueUpdateSelectedKeys = this.updateSelectedKeys
+    let vueUpdateSubscriptions = this.updateSubscriptions
+    this.$subscribeTo(activeHotAllColumnNames, async function(allNames) {
+      // let id = await vueActiveId()
+      let id = this.vueActiveId
+      console.log('id is:')
+      console.log(id)
+      let names = allNames[id]
+      vueUpdateSubscriptions(names, id)
+    })
+    this.initTableHeaderKeys()
+  }
+}
+</script>
---
+++ src/renderer/partials/ForeignKeys.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="foreignKeyFields">
+    <component v-for="keyType in ['tablekeys', 'tableheaderkeys']" :key="keyType" :is="keyType" :waitForHotIdFromTabId="waitForHotIdFromTabId" :propertyName="propertyName" :getProperty="getProperty" :getPropertyGivenHotId="getPropertyGivenHotId" :setProperty="setProperty"/>
+  </div>
+</template>
+<script>
+import tablekeys from '../partials/TableKeys'
+import tableheaderkeys from '../partials/TableHeaderKeys'
+export default {
+  components: {
+    tablekeys,
+    tableheaderkeys
+  },
+  name: 'foreignkeys',
+  props: ['waitForHotIdFromTabId', 'setProperty', 'getProperty', 'getPropertyGivenHotId', 'propertyName']
+}
+</script>
+<style lang="styl" scoped>
+@import '~static/css/foreignkeys'
+</style>
