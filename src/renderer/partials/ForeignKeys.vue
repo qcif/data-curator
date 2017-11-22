fd9f38cd0f517a49237de87363d2c862d7ebf105
Matthew Mulholland <matt@redboxresearchdata.com.au>
2017-11-23 09:39:55 +1000

    OCC-23: primary keys refactor for foreign keys introduction.

--- src/renderer/partials/ForeignKeys.vue
+++ src/renderer/partials/ForeignKeys.vue
@@ -12,7 +12,25 @@ export default {
     tableheaderkeys
   },
   name: 'foreignkeys',
-  props: ['waitForHotIdFromTabId', 'setProperty', 'getProperty', 'getPropertyGivenHotId', 'propertyName']
+  props: ['waitForHotIdFromTabId', 'setProperty', 'getProperty', 'getPropertyGivenHotId', 'propertyName'],
+  watch: {
+    selectedKeys: function(values) {
+      console.log('keys selected...')
+      this.setProperty(this.propertyName, values)
+    }
+  },
+  methods: {
+    updateSubscriptions: function(names, hotId) {
+      this.updateActiveNames(names)
+      this.updateSelectedKeys(hotId)
+    },
+    updateActiveNames: function(names) {
+      this.activeNames = _.without(names, '')
+    },
+    updateSelectedKeys: function(hotId) {
+      this.selectedKeys = this.getPropertyGivenHotId(this.propertyName, hotId) || []
+    }
+  }
 }
 </script>
 <style lang="styl" scoped>
--- src/renderer/rxSubject.js
+++ src/renderer/rxSubject.js
@@ -1,9 +1,9 @@
 import { Subject } from 'rxjs/Subject'
 
 let activeHotAllColumnNames = new Subject()
-let activeRxTab = new Subject()
+// let activeRxTab = new Subject()
 
 export {
-  activeHotAllColumnNames,
-  activeRxTab
+  activeHotAllColumnNames
+  // activeRxTab
 }
