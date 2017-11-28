4890426b2d2671c86f5776eebf63fa2b2e4b658e
Matthew Mulholland <matt@redboxresearchdata.com.au>
2017-11-28 13:31:27 +1000

    Fixes #228.

--- src/renderer/data-actions.js
+++ src/renderer/data-actions.js
@@ -1,6 +1,9 @@
 import tabStore from '../renderer/store/modules/tabs.js'
 import fs from 'fs'
 import {fixRaggedRows} from '@/ragged-rows.js'
+import {includeHeadersInData} from '@/frictionlessUtilities.js'
+// import parse from 'csv-parse/lib/sync'
+// import stringify from 'csv-stringify'
 const $ = global.jQuery = require('jquery/dist/jquery.js')
 require('jquery-csv/src/jquery.csv.js')
 
@@ -18,29 +21,55 @@ export function loadDataIntoHot(hot, data, format) {
 }
 
 export function saveDataToFile(hot, format, filename, callback) {
-  let data
   let tabId = tabStore.state.activeTab
   if (typeof filename === 'string') {
     tabStore.mutations.pushTabObject(tabStore.state, {id: tabId, filename: filename})
   } else {
     filename = _.get(tabStore.state.tabObjects, `${tabId}.filename`)
   }
-  // if no format specified, default to csv
-  if (typeof format === 'undefined') {
-    data = $.csv.fromArrays(hot.getData())
-  } else {
-    data = $.csv.fromArrays(hot.getData(), format.options)
-  }
   if (!filename) {
     return
   }
   if (typeof callback === 'undefined') {
-    fs.writeFile(filename, data, err => {
+    callback = (err) => {
       if (err) {
-        console.log(err.stack)
+        console.log('There was a problem saving data to file.')
+        throw err
       }
-    })
-  } else {
-    fs.writeFile(filename, data, callback)
+      console.log('File saved successfully.')
+    }
   }
+
+  // unlike handsontable, in frictionless by default, headers should be included (http://frictionlessdata.io/specs/csv-dialect)
+  let arrays = includeHeadersInData(hot)
+  let data
+  // if no format specified, default to csv
+  if (typeof format === 'undefined' || !format) {
+    data = $.csv.fromArrays(arrays)
+  } else {
+    data = $.csv.fromArrays(arrays, format.options)
+  }
+  // console.log('incoming hot arrays are:')
+  // console.log(hot.getData())
+  // console.log('outgoing csv data is')
+  // console.log(data)
+  fs.writeFile(filename, data, callback)
 }
+
+// TODO: once file formats updated to match use this function and remove dependency on jQuery csv
+// function csvToArrays(data, format) {
+//   var arrays = parse(data, format)
+//   return arrays
+// }
+
+// TODO: once file formats updated to match use this function and remove dependency on jQuery csv
+// function arraysToCsvToFile(arrays, format, filename, callback) {
+//   // TODO: now that using node.js csv 'formats' will need to be updated from jQuery csv to node.js csv
+//   stringify(arrays, format, function(err, data) {
+//     if (err) {
+//       console.log('There was a problem converting arrays to csv.')
+//       throw err
+//     }
+//     fs.writeFile(filename, data, callback)
+//   })
+// }
