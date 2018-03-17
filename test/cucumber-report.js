7c4cd59a33fc6f90fb7c76251fa94e046554cedb
Matthew Mulholland <matt@redboxresearchdata.com.au>
2018-03-17 22:57:22 +1000

    made expectation more flexible. removed logging. removed copied metadata.

--- test/cucumber-report.js
+++ test/cucumber-report.js
@@ -6,15 +6,7 @@ var options = {
   output: 'test/cucumber_report.html',
   reportSuiteAsScenarios: true,
   launchReport: true,
-  storeScreenshots: false,
-  metadata: {
-    'App Version': '0.3.2',
-    'Test Environment': 'STAGING',
-    'Browser': 'Chrome  54.0.2840.98',
-    'Platform': 'Windows 10',
-    'Parallel': 'Scenarios',
-    'Executed': 'Remote'
-  }
+  storeScreenshots: false
 }
 
 reporter.generate(options)
