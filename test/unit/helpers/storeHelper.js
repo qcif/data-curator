1f30a950f2302fb47512a3409a67f8951cde5f8d
Matthew Mulholland <matt@redboxresearchdata.com.au>
2018-03-18 12:58:31 +1000

    refactored vue for easily adding more tests.

--- test/unit/helpers/storeHelper.js
+++ test/unit/helpers/storeHelper.js
@@ -7,3 +7,28 @@ export function resetHotStore() {
     provenanceProperties: { markdown: '' }
   }
 }
+
+export function stubSimpleTabStore() {
+  const stubbedStore = {
+    state: {
+      tabs: [],
+      activeTab: '',
+      tabObjects: {},
+      tabIndex: -1,
+      activeTitle: ''
+    },
+
+    getters: {
+      getTabs: state => {
+        return 'stubbed tab'
+      },
+      getActiveTab: state => {
+        return 'stubbed active tab'
+      },
+      tabTitle: (state, getters) => (tabId) => {
+        return `stubbed title for ${tabId}`
+      }
+    }
+  }
+  return stubbedStore
+}
