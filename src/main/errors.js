b4e2f61783f1d96330fcf3746c4c68d3e3360498
Matthew Mulholland <matt@redboxresearchdata.com.au>
2018-03-23 21:38:53 +1000

    Added logic for showing errors on visible. Started logic for error window focus.

--- src/main/errors.js
+++ src/main/errors.js
@@ -2,4 +2,7 @@ import {focusOrNewSecondaryWindow} from './windows'
 
 export function showErrors() {
   let browserWindow = focusOrNewSecondaryWindow('errors', {width: 760, height: 400})
+  browserWindow.on('focus', (event) => {
+    console.log('in focus')
+  })
 }
