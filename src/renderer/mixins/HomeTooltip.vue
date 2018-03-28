5625d93e37877a7eabc2b4f9cff1368377760b1d
Matthew Mulholland <matt@redboxresearchdata.com.au>
2018-03-28 15:27:44 +1000

    #393: improved icon styling. Fixes #577.

--- src/renderer/mixins/HomeTooltip.vue
+++ src/renderer/mixins/HomeTooltip.vue
@@ -32,12 +32,6 @@ export default {
     },
     'tooltipNext': {
       template: '<div id="tooltip-next">Next Column Property</div>'
-    },
-    'tooltipOpenErrorsWindow': {
-      template: '<div id="tooltip-open-errors-window">Open Errors in a New Window</div>'
-    },
-    'tooltipWriteErrorsProvenance': {
-      template: '<div id="tooltip-write-errors-provenance">Write Errors to Provenance Information</div>'
     }
   }
 }
