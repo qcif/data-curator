66d83ed479f957945e4a9c0a27120a1d673d6833
Matthew Mulholland <matt@redboxresearchdata.com.au>
2018-05-15 19:54:25 +1000

    #283: reverted libraries until some issues resolved (e.g., capture image).

--- test/features/support/hooks.js
+++ test/features/support/hooks.js
@@ -4,17 +4,14 @@ import { After, Before, Status, Given, When, Then } from 'cucumber'
 import fakeDialog from 'spectron-fake-dialog'
 import { expect, should, assert } from 'chai'
 
-After(async function (testCase) {
+After({timeout: 10000}, async function (testCase) {
   if (testCase.result.status === Status.FAILED) {
     const imageBuffer = await this.app.browserWindow.capturePage()
     await this.attach(imageBuffer, 'image/png')
-  } else {
+  }
     if (this.app && this.app.isRunning()) {
-      // const imageBuffer = await this.app.browserWindow.capturePage()
-      // await this.attach(imageBuffer, 'image/png')
       await this.app.stop()
     }
-  }
 })
 
 Before({timeout: 10000}, async function () {
@@ -23,7 +20,6 @@ Before({timeout: 10000}, async function () {
   this.pageTimeout = 5000
   fakeDialog.apply(this.app)
   await this.app.start()
-  // await this.app.client.waitUntilWindowLoaded()
   // Auto-close message dialog : 1 = Quit (No Cancel No save)
   fakeDialog.mock([{method: 'showMessageBox', value: 1}])
   fakeDialog.mock([{method: 'showOpenDialog', value: this.openFileDialogReturned}])
