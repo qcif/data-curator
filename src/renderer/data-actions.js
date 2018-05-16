d810aec72dd3946e4179baa5981ce5404c4b642e
Matthew Mulholland <matt@redboxresearchdata.com.au>
2018-05-16 10:04:21 +1000

    #651: handle bad excel sheets.

--- src/renderer/data-actions.js
+++ src/renderer/data-actions.js
@@ -18,28 +18,25 @@ export function loadDataIntoHot(hot, data, format) {
 }
 
 export function loadCsvDataIntoHot(hot, data, format) {
-  try {
-    let arrays
-    // if no format specified, default to csv
-    if (typeof format === 'undefined' || !format) {
-      arrays = parse(data)
-    } else {
-      let csvOptions = dialectToCsvOptions(format.dialect)
-      // let csv parser handle the line terminators
-      _.unset(csvOptions, 'rowDelimiter')
-      // TODO: update to stream
-      arrays = parse(data, csvOptions)
-      pushCsvFormat(hot.guid, format)
-    }
-
-    fixRaggedRows(arrays)
-    hot.loadData(arrays)
-    hot.render()
-    // frictionless csv header default = true
-    toggleHeaderNoFeedback(hot)
-  } catch (error) {
-    console.log('There was a problem loading data', error)
+  // do not handle errors here as caller can activate appropriate user feedback dialog
+  let arrays
+  // if no format specified, default to csv
+  if (typeof format === 'undefined' || !format) {
+    arrays = parse(data)
+  } else {
+    let csvOptions = dialectToCsvOptions(format.dialect)
+    // let csv parser handle the line terminators
+    _.unset(csvOptions, 'rowDelimiter')
+    // TODO: update to stream
+    arrays = parse(data, csvOptions)
+    pushCsvFormat(hot.guid, format)
   }
+
+  fixRaggedRows(arrays)
+  hot.loadData(arrays)
+  hot.render()
+  // frictionless csv header default = true
+  toggleHeaderNoFeedback(hot)
 }
 
 export function loadArrayDataIntoHot(hot, arrays, format) {
