35334af2e722955fa391fd8755d6c6737e0a4b55
Matthew Mulholland <matt@redboxresearchdata.com.au>
2018-06-20 11:03:07 +1000

    poc for post report script to tally stats.

--- test/features/support/hooks.js
+++ test/features/support/hooks.js
@@ -1,9 +1,47 @@
 import { Application } from 'spectron'
 import electron from 'electron'
-import { After, Before, Status, Given, When, Then } from 'cucumber'
+import { After, Before, Status, Given, When, Then, AfterAll} from 'cucumber'
 import fakeDialog from 'spectron-fake-dialog'
 import { expect, should, assert } from 'chai'
-const { exec } = require('child_process')
+import {exec} from 'child_process'
+import fs from 'fs-extra'
+import { formatSummary } from 'cucumber/lib/formatter/helpers/summary_helpers.js'
+import getColorFns from 'cucumber/lib/formatter/get_color_fns'
+// var jsonfile = require('jsonfile')
+import etl from 'etl'
+const _ = require('lodash')
+
+const IS_ISSUE = {
+  [Status.AMBIGUOUS]: true,
+  [Status.FAILED]: true,
+  [Status.PASSED]: false,
+  [Status.PENDING]: true,
+  [Status.SKIPPED]: false,
+  [Status.UNDEFINED]: true
+}
+
+const failures = []
+const warnings = []
+const success = []
+
+// function isIssue(status) {
+//   return IS_ISSUE[status]
+// }
+
+function isTestCaseFailure(testCase) {
+  return _.includes([Status.AMBIGUOUS, Status.FAILED], testCase.result.status)
+}
+
+function isTestCaseSuccess(testCase) {
+  return _.includes([Status.PASSED], testCase.result.status)
+}
+
+function isTestCaseWarning(testCase) {
+  return _.includes(
+    [Status.PENDING, Status.UNDEFINED],
+    testCase.result.status
+  )
+}
 
 async function stopAppRunning(app) {
   try {
@@ -38,6 +76,8 @@ function tallyTestAppveyor(testCase) {
 After({timeout: 40000}, async function (testCase) {
   try {
     // console.log('Starting after hook....')
+    // console.log(testCase)
+    // console.log(testCase.pickle.steps[0])
     if (testCase.result.status === Status.FAILED) {
       if (this.app && this.app.browserWindow) {
         const imageBuffer = await this.app.browserWindow.capturePage()
@@ -45,6 +85,15 @@ After({timeout: 40000}, async function (testCase) {
         // console.log('got attachment in', attachResult)
       }
     }
+    // if (isTestCaseFailure(testCase)) {
+    //   failures.push(testCase)
+    // } else if (isTestCaseWarning(testCase)) {
+    //   warnings.push(testCase)
+    // } else if (isTestCaseSuccess(testCase)) {
+    //   success.push(testCase)
+    // } else {
+    //   console.log('nothing pushed')
+    // }
     tallyTestAppveyor(testCase)
     // stopAppRunning(this.app)
   } catch (error) {
@@ -53,6 +102,53 @@ After({timeout: 40000}, async function (testCase) {
     await stopAppRunning(this.app)
   }
 })
+
+// AfterAll(async function () {
+//   let result
+//   try {
+//     console.log('Starting after all hook....')
+//     console.log(`failures are`, failures)
+//     console.log(`warnings are`, warnings)
+//     console.log(`success are`, success)
+//     // const jsonPath = require('path').join(__dirname, '../../cucumber_report.json')
+//     // let testCaseMap = fs.readFileSync(jsonPath)
+//     // console.log(`path is`, jsonPath)
+//     // let testCaseMap = {}
+//     // let testCaseMap = jsonfile.readFileSync(jsonPath)
+//     // let testCaseMap = await jsonfile.readFile(jsonPath)
+//     // fs.readFile(jsonPath, 'utf8', function readFileCallback(err, data) {
+//     //   if (err) {
+//     //     console.log(err)
+//     //   } else {
+//     //     let obj = JSON.parse(data) // now it an object
+//     //     console.log(`next obj is`, obj)
+//     //   }
+//     // })
+//     // let value = await etl.file(jsonPath)
+//     //   .pipe(etl.stringify())
+//     //   .promise()
+//     // // etl always returns an array
+//     // if (_.isEmpty(value)) {
+//     //   throw new Error(`Unable to find text in filename: ${jsonPath}`)
+//     // }
+//     // let testCaseMap = JSON.parse(value).text
+//     // // let testCaseMap = {}
+//     // console.log(`test case map is`, testCaseMap)
+//     // let testRun = { result: { duration: 0 } }
+//     // let cucumberSummaryOptions = {
+//     //   colorFns: getColorFns(false),
+//     //   testCaseMap: testCaseMap,
+//     //   testRun: testRun
+//     // }
+//     // result = formatSummary(cucumberSummaryOptions)
+//   } catch (error) {
+//     console.log('error in after hook', error)
+//   } finally {
+//     // await stopAppRunning(this.app)
+//     console.log(`final result is`)
+//     console.log(result)
+//   }
+// })
 
 Before({timeout: 20000}, async function (testCase) {
   try {
--- test/postreport.js
+++ test/postreport.js
@@ -1,11 +1,32 @@
 #!/usr/bin/env node
 'use strict'
-var fs = require('fs')
-var _ = require('lodash')
+var fs = require('fs-extra')
+// var formatSummary = require('cucumber/lib/formatter/helpers/summary_helpers.js')
+// import getColorFns from 'cucumber/lib/formatter/get_color_fns'
+// var jsonfile = require('jsonfile')
+var etl = require('etl')
+const _ = require('lodash')
 var glob = require('glob')
+var jsonfile = require('jsonfile')
 
-glob.sync('build/*.@(zip|dmg|exe)').forEach(file => {
-  console.log(file)
-  var renamedFile = _.replace(file, ' ', '')
-  fs.renameSync(file, renamedFile)
-})
+const jsonPath = require('path').join(__dirname, 'cucumber_report.json')
+let testCaseMap = jsonfile.readFileSync(jsonPath)
+// let value = etl.file(jsonPath)
+//   .pipe(etl.stringify())
+//   .promise()
+//   .then(function() {
+//     // etl always returns an array
+//     if (_.isEmpty(value)) {
+//       throw new Error(`Unable to find text in filename: ${jsonPath}`)
+//     }
+//     let testCaseMap = JSON.parse(value).text
+//     console.log('test case')
+//     console.log(testCaseMap)
+//   })
+
+console.log('test case')
+console.log(testCaseMap)
+for (let testCase of testCaseMap) {
+  console.log('next')
+  console.log(testCase)
+}
