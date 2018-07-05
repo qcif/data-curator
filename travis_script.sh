1e22d82106e2e5ca4bce29075d4156b17393512d
Matthew Mulholland <matt@redboxresearchdata.com.au>
2018-07-05 16:37:37 +1000

    Try running travis test with current user.

--- travis_script.sh
+++ travis_script.sh
@@ -6,7 +6,11 @@ if [ "${TRAVIS_BRANCH}" = "ci" ]; then
   yarn run unit
   yarn run unit:coverage
   sudo rm -Rf rm /tmp/.X*
-  sudo Xvfb :99 -ac -screen 0 1024x768x8 && yarn run e2e
+  export DISPLAY=':99.0'
+  # sudo Xvfb :99 -ac -screen 0 1024x768x8
+  Xvfb :99 -screen 0 1024x768x24 > /dev/null 2>&1 &
+  yarn run e2e
 else
   yarn run release:predraft
 fi
+set +e
