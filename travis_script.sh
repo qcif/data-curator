816a6b86beac8b0da209ebab623e870ff8aa250b
Matthew Mulholland <matt@redboxresearchdata.com.au>
2018-07-05 18:15:30 +1000

    attempt parallel tests on travis.

--- travis_script.sh
+++ travis_script.sh
@@ -3,14 +3,11 @@ set -ev
 unset -f cd
 shell_session_update() { :; }
 if [ "${TRAVIS_BRANCH}" = "ci" ]; then
-  yarn run unit
-  yarn run unit:coverage
   sudo rm -Rf rm /tmp/.X*
   export DISPLAY=':99.0'
-  # sudo Xvfb :99 -ac -screen 0 1024x768x8
   Xvfb :99 -screen 0 1024x768x24 > /dev/null 2>&1 &
-  yarn run e2e
-else
-  yarn run release:predraft
+  yarn run clean && yarn run pack && yarn run cucumber:postpack:witharg $@
+# else
+#   yarn run release:predraft
 fi
 set +e
