7db6133b1e5019d9648f4849c5eabf9206849bd2
Matthew Mulholland <matt@redboxresearchdata.com.au>
2018-10-02 10:03:15 +1000

    merged ci.

--- travis_script.sh
+++ travis_script.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 set -ev
+unset -f cd
+shell_session_update() { :; }
 if [ "${TRAVIS_BRANCH}" == "ci" ] || [ "${TRAVIS_BRANCH}" == "testci" ]; then
-  yarn run unit
-  yarn run unit:coverage
-else
-  yarn run release:predraft
+  sudo rm -Rf rm /tmp/.X*
+  export DISPLAY=':99.0'
+  Xvfb :99 -screen 0 1024x768x24 > /dev/null 2>&1 &
+  yarn run clean && yarn run pack && yarn run cucumber:postpack:witharg $@
+# else
+#   yarn run release:predraft
 fi
+set +e
