a629db7182c100fb8f6efc9e9f6cccba6ae483a3
Matthew Mulholland <matt@redboxresearchdata.com.au>
2018-10-03 13:49:37 +1000

    testing webpack and babel upgrade with webpack 4 support in config.

--- .electron-vue/build.js
+++ .electron-vue/build.js
@@ -81,6 +81,7 @@ function build () {
 
 function pack (config) {
   return new Promise((resolve, reject) => {
+    config.mode = 'production'
     webpack(config, (err, stats) => {
       if (err) reject(err.stack || err)
       else if (stats.hasErrors()) {
