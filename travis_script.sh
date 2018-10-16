3fb4093e2dbd7c576f74f51fb419f0941c622931
Matthew Mulholland <matt@redboxresearchdata.com.au>
2018-10-16 10:57:45 +1000

    resolved merge conflicts from testci.

--- travis_script.sh
+++ travis_script.sh
@@ -2,11 +2,7 @@
 set -ev
 unset -f cd
 shell_session_update() { :; }
-<<<<<<< HEAD
-if [ "${TRAVIS_BRANCH}" = "ci" ]; then
-=======
 if [ "${TRAVIS_BRANCH}" == "ci" ] || [ "${TRAVIS_BRANCH}" == "testci" ]; then
->>>>>>> testci
   sudo rm -Rf rm /tmp/.X*
   export DISPLAY=':99.0'
   Xvfb :99 -screen 0 1024x768x24 > /dev/null 2>&1 &
