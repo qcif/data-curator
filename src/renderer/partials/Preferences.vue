066724f4d4e67459cc4f8402309e4f575df1dfab
Matthew Mulholland <matt@redboxresearchdata.com.au>
2018-10-17 22:15:37 +1000

    rolled back poc from 843.

--- src/renderer/partials/Preferences.vue
+++ src/renderer/partials/Preferences.vue
@@ -9,20 +9,6 @@
                    :getProperty="getProperty" :getPropertyGivenHotId="getPropertyGivenHotId"
                    :setProperty="setProperty" :waitForHotIdFromTabId="waitForHotIdFromTabId"
                    :currentHotId="currentHotId" :is="formprop.key" :contributorsSetter="contributorsSetter"/>
-        <div v-else-if="isProxyLabel(formprop.label)" id="proxy-preferences" class="inputs-container">
-          <div v-show="isProxyVisible" class="input-group" v-for="(attribute, index) in formprop.attributes">
-            <span class="input-group-addon input-sm col-sm-4">{{upperCase(attribute)}}</span>
-            <input type="text" :class="{ 'form-control input-sm col-sm-8': true }"
-                   :id="proxyKey(attribute)" :value="getProperty(proxyKey(attribute))" @input="setProperty(proxyKey(attribute), $event.target.value)"/>
-          </div>
-          <div class="button-container">
-            <button type="button" class="toggle-proxy btn btn-primary btn-sm" @click="isProxyVisible=!isProxyVisible">
-              <span class="glyphicon glyphicon-plus"/>{{isProxyVisible ? 'Hide Proxy' : 'Show Proxy'}}
-            </button>
-          </div>
-        </div>
-
-        <!--<input v-else type="text" :class="{ 'form-control input-sm col-sm-9': true, 'validate-danger': errors.has(formprop.key) }" :id="formprop.key" :value="getProperty(formprop.key)" @input="setProperty(formprop.key, $event.target.value)" v-validate="validationRules(formprop.key)" :name="formprop.key"/>-->
         <div v-show="errors.has(formprop.key) && removeProperty(formprop.key)" class="row help validate-danger">
           {{ errors.first(formprop.key)}}
         </div>
@@ -37,7 +23,6 @@
   import PreferencesTooltip from '@/mixins/PreferencesTooltip'
   import ValidationRules from '@/mixins/ValidationRules'
   import {ipcRenderer as ipc} from 'electron'
-  import {proxyAttributes, proxyKeyName} from '@/proxyProperties.js'
 
   export default {
     extends: SideNav,
@@ -49,7 +34,6 @@
     },
     data() {
       return {
-        isProxyVisible: false,
         formprops: [
           {
             label: 'License(s)',
@@ -62,10 +46,6 @@
             key: 'contributors',
             tooltipId: 'tooltip-preferences-contributors',
             tooltipView: 'tooltipPreferencesContributors'
-          },
-          {
-            label: 'Proxy',
-            attributes: proxyAttributes
           }]
       }
     },
@@ -98,16 +78,9 @@
         let value = ''
         this.setProperty(key, value)
         return true
-      },
-      isProxyLabel: function (label) {
-        return _.lowerCase(label) === 'proxy'
-      },
-      upperCase: function(value) {
-        return _.upperCase(value)
-      },
-      proxyKey: function(key) {
-        return proxyKeyName(key)
       }
+      },
+    created: function () {
     }
   }
 </script>
@@ -116,7 +89,4 @@
 </style>
 <style lang="styl" scoped>
   @import '~static/css/sidenav'
-</style>
-<style lang="styl" scoped>
-  @import '~static/css/preferences'
 </style>
