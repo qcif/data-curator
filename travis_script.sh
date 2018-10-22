e408a97d859a1fc1add8a4437e45f0aafc96c21d
Matthew Mulholland <matt@redboxresearchdata.com.au>
2018-10-22 10:14:30 +1000

    added stash and yarn before test and ci to ensure have merged latest develop and its dependencies correctly.

--- travis_script.sh
+++ travis_script.sh
@@ -3,11 +3,6 @@ set -ev
 unset -f cd
 shell_session_update() { :; }
 if [ "${TRAVIS_BRANCH}" == "ci" ] || [ "${TRAVIS_BRANCH}" == "testci" ]; then
-  git remote set-branches --add origin develop || exit
-  git fetch origin develop || exit
-  git checkout --track origin/develop || exit
-  git checkout ${TRAVIS_BRANCH} || exit
-  git merge develop || exit
   sudo rm -Rf rm /tmp/.X*
   export DISPLAY=':99.0'
   Xvfb :99 -screen 0 1024x768x24 > /dev/null 2>&1 &
