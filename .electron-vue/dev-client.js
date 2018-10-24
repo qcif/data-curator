0a6d6b252fa4fab02d54d29b8929a00c38ae3fc4
Matthew Mulholland <matt@redboxresearchdata.com.au>
2018-10-25 08:10:25 +1000

    ensure errors initialised correctly when errors are present.

--- .electron-vue/dev-client.js
+++ .electron-vue/dev-client.js
@@ -1,7 +1,6 @@
 const hotClient = require('webpack-hot-middleware/client?noInfo=true&reload=true')
 
 hotClient.subscribe(event => {
-  /**
   if (event.action === 'reload') {
     window.location.reload()
   }
