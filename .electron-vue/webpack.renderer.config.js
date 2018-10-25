3a3c51f9dfbf549775b524a01ed33c78cb99f134
Matthew Mulholland <matt@redboxresearchdata.com.au>
2018-10-25 13:49:40 +1000

    updated to follow library name change.

--- .electron-vue/webpack.renderer.config.js
+++ .electron-vue/webpack.renderer.config.js
@@ -72,7 +72,7 @@ let rendererConfig = {
         use: {
           loader: 'eslint-loader',
           options: {
-            formatter: require('eslint-friendly-formatter')
+            formatter: require('eslint-formatter-friendly')
           }
         }
       },
