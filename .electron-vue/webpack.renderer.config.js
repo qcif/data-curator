4955217a167b1bf646156a640ebe84a2945c3053
Matthew Mulholland <matt@redboxresearchdata.com.au>
2018-10-25 13:48:28 +1000

    added back linter for webpack once all linted. added comment for removing rule check.

--- .electron-vue/webpack.renderer.config.js
+++ .electron-vue/webpack.renderer.config.js
@@ -65,17 +65,17 @@ let rendererConfig = {
   } : {},
   module: {
     rules: [
-      // {
-      //   test: /\.(js|vue)$/,
-      //   enforce: 'pre',
-      //   exclude: /node_modules/,
-      //   use: {
-      //     loader: 'eslint-loader',
-      //     options: {
-      //       formatter: require('eslint-formatter-friendly')
-      //     }
-      //   }
-      // },
+      {
+        test: /\.(js|vue)$/,
+        enforce: 'pre',
+        exclude: /node_modules/,
+        use: {
+          loader: 'eslint-loader',
+          options: {
+            formatter: require('eslint-friendly-formatter')
+          }
+        }
+      },
       {
         test: /\.css$/,
         use: [
