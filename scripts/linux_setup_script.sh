b05c63536df7890dff9c0fd9940ef6108f7d5523
mattRedBox <matt@redboxresearchdata.com.au>
2018-11-04 03:09:01 +0000

    added dependencies for X in linux.

--- scripts/linux_setup_script.sh
+++ scripts/linux_setup_script.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
-sudo apt-get install -y libgtk-3-0
+sudo apt-get update -y
+#sudo apt-get install -y libgtk-3-0
+sudo apt-get install -y libgtkextra-dev libgconf2-dev libnss3 libasound2 libxtst-dev
