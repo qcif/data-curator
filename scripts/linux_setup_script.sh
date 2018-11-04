1aa5bf1a5f2ebf410724575e99cedfd01c7b46d3
Matthew Mulholland <matt@redboxresearchdata.com.au>
2018-11-04 15:25:47 +1000

    ci: attempt travis for linux with minimal installation.

--- scripts/linux_setup_script.sh
+++ scripts/linux_setup_script.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 sudo apt-get update -y
 #sudo apt-get install -y libgtk-3-0
-sudo apt-get install -y libgtkextra-dev libgconf2-dev libnss3 libasound2 libxtst-dev
+#sudo apt-get install -y libgtkextra-dev libgconf2-dev libnss3 libasound2 libxtst-dev
+#sudo apt-get install -y libgtk-3-dev libgconf2-dev libnss3 libasound2 libxtst-dev
+
+sudo apt-get install -y libgtk-3-dev libgconf2-dev
