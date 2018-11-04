ebe280fb31dee9c7b4099b941803f4ec7bb6b850
Matthew Mulholland <matt@redboxresearchdata.com.au>
2018-11-05 09:24:07 +1000

    ci: attempt travis unit test in headless.

--- scripts/linux_setup_script.sh
+++ scripts/linux_setup_script.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
+set -ev
+unset -f cd
+shell_session_update() { :; }
+sudo rm -Rf rm /tmp/.X*
+export DISPLAY=':99.0'
+Xvfb :99 -screen 0 1024x768x24 > /dev/null 2>&1 &
 sudo apt-get update -y
 
 sudo apt-get install -y libgtk-3-dev libgconf2-dev libnss3 libasound2 libxtst-dev
+set +e
