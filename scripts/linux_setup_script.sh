59da0ffac719795141b624c8205f462c0b79ba8c
mattRedBox <matt@redboxresearchdata.com.au>
2018-11-05 03:00:44 +0000

    ci: attempt headless for linux.

--- scripts/linux_setup_script.sh
+++ scripts/linux_setup_script.sh
@@ -1,3 +1,16 @@
 #!/usr/bin/env bash
-sudo apt-get update -y
-sudo apt-get install -y libgtk-3-dev libgconf2-dev libnss3 libasound2 libxtst-dev libx11-xcb-dev libxss-dev
+set -ev
+unset -f cd
+shell_session_update() { :; }
+if [ "${TRAVIS_BRANCH}" == "ci" ] || [ "${TRAVIS_BRANCH}" == "testci" ]; then
+  sudo rm -Rf rm /tmp/.X*
+  export DISPLAY=':99.0'
+  Xvfb :99 -screen 0 1024x768x24 > /dev/null 2>&1 &
+  sudo apt-get update -y
+  sudo apt-get install -y libgtk-3-dev libgconf2-dev libnss3 libasound2 libxtst-dev libx11-xcb-dev libxss-dev
+  yarn run clean
+  yarn run unit
+fi
+set +e
+
+
