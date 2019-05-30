bf38858ab3dfb7ec691b6b461856a7a3ca86a0dd
Matthew Mulholland <matt@redboxresearchdata.com.au>
2019-05-30 14:52:21 +1000

    moved display export so that PR builds might pick up DISPLAY for headless better.

--- scripts/travis_headless_e2e_tests.sh
+++ scripts/travis_headless_e2e_tests.sh
@@ -3,7 +3,7 @@ set -ev
 unset -f cd
 shell_session_update() { :; }
 sudo rm -Rf rm /tmp/.X*
-export DISPLAY=':99.0'
 Xvfb :99 -screen 0 1024x768x24 > /dev/null 2>&1 &
+export DISPLAY=':99.0'
 yarn run clean && yarn run pack && yarn run cucumber:postpack:witharg $@
 set +e
