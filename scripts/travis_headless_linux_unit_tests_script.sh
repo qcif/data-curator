1b49fe91b3d018756ce743c8d31b005f89baa5c4
mattRedBox <matt@redboxresearchdata.com.au>
2020-05-12 01:30:29 +1000

    Attempt travis xvfb wrapper.

--- scripts/travis_headless_linux_unit_tests_script.sh
+++ scripts/travis_headless_linux_unit_tests_script.sh
@@ -4,7 +4,5 @@ unset -f cd
 echo "executing $0"
 shell_session_update() { :; }
 sudo rm -Rf rm /tmp/.X*
-Xvfb :99 -screen 0 1024x768x24 > /dev/null 2>&1 &
-export DISPLAY=':99.0'
-yarn run unit
+xvfb-run --server-args="-screen 0 1024x768x24" yarn run unit
 set +e
