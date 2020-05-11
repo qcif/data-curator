7b3d4a1d14ac6d4999a9035d887771b74967e559
mattRedBox <matt@redboxresearchdata.com.au>
2020-05-11 13:36:17 +1000

    Corrected script use in unit tests on travis.

--- scripts/travis_headless_linux_unit_tests_script.sh
+++ scripts/travis_headless_linux_unit_tests_script.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-source ./linux_install_os_dependencies_for_testing_on_ubuntu.sh
 set -ev
 unset -f cd
 echo "executing $0"
@@ -7,6 +6,5 @@ shell_session_update() { :; }
 sudo rm -Rf rm /tmp/.X*
 Xvfb :99 -screen 0 1024x768x24 > /dev/null 2>&1 &
 export DISPLAY=':99.0'
-chmod +x
 yarn run unit
 set +e
