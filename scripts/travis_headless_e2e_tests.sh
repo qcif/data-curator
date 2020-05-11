0427e67df00f51b331c7e0b3ec7f27958bef5c87
mattRedBox <matt@redboxresearchdata.com.au>
2020-05-12 02:13:41 +1000

    Attempt travis xvfb wrapper.

--- scripts/travis_headless_e2e_tests.sh
+++ scripts/travis_headless_e2e_tests.sh
@@ -2,11 +2,12 @@
 set -ev
 unset -f cd
 shell_session_update() { :; }
-sudo rm -Rf rm /tmp/.X*
+#sudo rm -Rf rm /tmp/.X*
 #export DISPLAY=:99.0
 #sh -e /etc/init.d/xvfb start
 #Xvfb :99 -screen 0 1024x768x24 > /dev/null 2>&1 &
 #export DISPLAY=':99.0'
 #sleep 5
-yarn run clean && yarn run pack && xvfb-run -a --server-args="-screen 0 1024x768x24" yarn run cucumber:postpack:witharg $@
+#"/sbin/start-stop-daemon --start --quiet --pidfile /tmp/custom_xvfb_99.pid --make-pidfile --background --exec /usr/bin/Xvfb -- :99 -ac -screen 0 1280x1024x16"
+yarn run clean && yarn run pack && yarn run cucumber:postpack:witharg $@
 set +e
--- scripts/travis_headless_linux_unit_tests_script.sh
+++ scripts/travis_headless_linux_unit_tests_script.sh
@@ -3,10 +3,11 @@ set -ev
 unset -f cd
 echo "executing $0"
 shell_session_update() { :; }
-sudo rm -Rf rm /tmp/.X*
+#sudo rm -Rf rm /tmp/.X*
 #export DISPLAY=:99.0
 #sh -e /etc/init.d/xvfb start
 #Xvfb :99 -screen 0 1024x768x24 > /dev/null 2>&1 &
 #export DISPLAY=':99.0'
-xvfb-run -a --server-args="-screen 0 1024x768x24" yarn run unit
+#xvfb-run -a --server-args="-screen 0 1024x768x24" yarn run unit
+yarn run unit
 set +e
