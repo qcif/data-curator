41c474dea4a4a10727db0c64fde157c17795f0ea
mattRedBox <matt@redboxresearchdata.com.au>
2020-05-14 09:35:03 +1000

    revert config to last successful e2e on travis.

--- scripts/travis_headless_linux_unit_tests_script.sh
+++ scripts/travis_headless_linux_unit_tests_script.sh
@@ -3,11 +3,10 @@ set -ev
 unset -f cd
 echo "executing $0"
 shell_session_update() { :; }
-#sudo rm -Rf rm /tmp/.X*
-#export DISPLAY=:99.0
-#sh -e /etc/init.d/xvfb start
-#Xvfb :99 -screen 0 1024x768x24 > /dev/null 2>&1 &
-#export DISPLAY=':99.0'
-#xvfb-run -a --server-args="-screen 0 1024x768x24" yarn run unit
+sudo rm -Rf rm /tmp/.X*
+Xvfb :99 -screen 0 1024x768x24 > /dev/null 2>&1 &
+export DISPLAY=':99.0'
+#sudo apt-get update -y
+#sudo apt-get install -y libgtk-3-dev libgconf2-dev libnss3 libasound2 libxtst-dev libx11-xcb-dev libxss-dev
 yarn run unit
 set +e
