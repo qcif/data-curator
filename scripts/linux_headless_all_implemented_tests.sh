099e00a8d1b9f3f93452f0c8cfa5ce492bb84d67
mattRedBox <matt@redboxresearchdata.com.au>
2020-05-15 23:46:30 +1000

    Correct and simplify CI.

--- scripts/linux_headless_all_implemented_tests.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#source ./linux_install_os_dependencies_for_testing_on_ubuntu.sh
-Xvfb :99 -screen 0 1024x768x24 > /dev/null 2>&1 &
-export DISPLAY=':99.0'
-yarn run unit
-export NODE_OPTIONS="--max-old-space-size=8192"
-#yarn run clean && yarn run pack && yarn run cucumber:postpack:impl
-yarn run clean && yarn run pack && yarn run cross-env BABEL_ENV=test cucumber-js --require-module @babel/register --tags @impl -f json:test/cucumber_report.json test/features/edit/insert-column-after.feature
--- scripts/travis_headless_linux_unit_tests_script.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -ev
-unset -f cd
-echo "executing $0"
-shell_session_update() { :; }
-sudo rm -Rf rm /tmp/.X*
-xvfb-run --server-args="-screen 0 1024x768x24" yarn run unit
-set +e
