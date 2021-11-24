fc9d2b848fcb77528357f908d9cdda1f3be9354f
mattRedBox <matt@redboxresearchdata.com.au>
2021-11-25 08:19:57 +1000

    Setup next dev audit.

--- test/features/support/help/help_steps.js
+++ test/features/support/help/help_steps.js
@@ -5,6 +5,7 @@ import { sendOpenUrlCall } from '../page-objects/helpers.js'
 // TODO: complete this test
 Then(/^a call to open an external url (?:should be|is) made/, async function () {
   let value = await sendOpenUrlCall('https://qcif.github.io/data-curator-help')
+  console.log(`value is ${value}`)
   const resultlogs = await this.app.client.getMainProcessLogs()
   for (let log of resultlogs) {
     console.dir(log)
