919b74565bacfad44efcde31018b122689b35b61
mattRedBox <matt@redboxresearchdata.com.au>
2021-11-24 13:13:42 +1000

    Added back stub for test to complete.

--- test/features/support/help/help_steps.js
+++ test/features/support/help/help_steps.js
@@ -2,11 +2,12 @@ import { expect } from 'chai'
 import { Then } from 'cucumber'
 import { sendOpenUrlCall } from '../page-objects/helpers.js'
 
-// Then(/^a call to open an external url (?:should be|is) made/, async function () {
-//   let value = await sendOpenUrlCall('https://qcif.github.io/data-curator-help')
-//   const resultlogs = await this.app.client.getMainProcessLogs()
-//   for (let log of resultlogs) {
-//     console.dir(log)
-//   }
-//   expect(value).to.equal(true)
-// })
+// TODO: complete this test
+Then(/^a call to open an external url (?:should be|is) made/, async function () {
+  let value = await sendOpenUrlCall('https://qcif.github.io/data-curator-help')
+  const resultlogs = await this.app.client.getMainProcessLogs()
+  for (let log of resultlogs) {
+    console.dir(log)
+  }
+  expect(value).to.equal(true)
+})
